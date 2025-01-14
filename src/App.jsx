f439f43923ce3e6eacd980d9ae02e56478971871
JensonAquino <jensonaquino2622@gmail.com>
2025-01-15 00:19:09 +0100

    feat: search bar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import AppHeader from "./components/AppHeader"
-import AppMain from "./components/AppMain"
-
-
+import AppHeader from "./components/AppHeader";
+import AppMain from "./components/AppMain";
+import axios from "axios";
+const apiKey = "9d1063d346243d3931ce87274e56c0a6";
+const apiUrl = "https://api.themoviedb.org/3";
 function App() {
-
+  
   return (
     <>
       <AppHeader/>
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,15 +1,39 @@
-function AppHeader () {
+import axios from "axios";
+import { useState } from "react";
+
+const apiKey = "9d1063d346243d3931ce87274e56c0a6";
+const apiUrl = "https://api.themoviedb.org/3";
+
+
+function AppHeader() {
+
+    const [searchValue, setSearchValue] = useState("")
+
+    function getMovies() {
+
+        axios.get(`${apiUrl}/search/movie`, {
+            params: {
+                api_key: apiKey,
+                query: searchValue,
+            },
+        })
+            .then((resp) => {
+                console.log(resp);
+
+            });
+    }
+
     return (
-        <div>
-            <p>
-                logo
+        <div className="header">
+            <p className="logo">
+                BOOLFIX
             </p>
             <section>
-                <input type="search"  />
-                <button></button>
+                <input type="search" value={searchValue} onChange={(event) => setSearchValue(event.target.value)} />
+                <button onClick={getMovies}>Cerca</button>
             </section>
         </div>
-    
+
     )
 }
 
